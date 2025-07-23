489c2cd4ff23dfe69e67430a7902db57c6f564b2
Nolan McGill <nomcgill@gmail.com>
2025-07-23 13:07:43 -0500

    Moved app folder out from the redundant src folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <h1>Hello World</h1>
         {children}
       </body>
     </html>
